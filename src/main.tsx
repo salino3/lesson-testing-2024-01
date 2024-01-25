f976ac361de2675f896bca76be3f0536c60a6db6
salino3 <programacionflavio@gmail.com>
2024-01-25 15:56:37 +0100

    configuration done

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import './index.css'
 
 // npm install --save-dev jest-svg-transformer
 // npm install --save-dev @babel/core @babel/preset-typescript
+// npm install --save-dev @testing-library/dom
+// npm install --save-dev @testing-library/react @testing-library/dom
+
 
 
 
