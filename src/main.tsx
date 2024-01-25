fd7e7e5f38cbf37b6925fb37f65ac76fa5e9fbda
salino3 <programacionflavio@gmail.com>
2024-01-25 16:04:53 +0100

    1º test

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+// npm i --dev jest babel-jest @babel/preset-env @babel/preset-react 
 // npm install --save-dev jest-svg-transformer
 // npm install --save-dev @babel/core @babel/preset-typescript
 // npm install --save-dev @testing-library/dom
 // npm install --save-dev @testing-library/react @testing-library/dom
-
+// npm install --dev @testing-library/react @testing-library/dom @testing-library/user-event @types/jest jest-environment-jsdom
+// npm install --save-dev @babel/preset-env
+// npm i --save-dev identity-obj-proxy
 
 
 
